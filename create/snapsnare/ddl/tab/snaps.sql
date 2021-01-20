4e062d81c1c6aa92aed392959a1d0c8c74e13e61
Jan Ripke <janripke@gmail.com>
2021-01-20 22:05:53 +0100

    added chord_schema

--- create/snapsnare/ddl/tab/snaps.sql
+++ create/snapsnare/ddl/tab/snaps.sql
@@ -1,13 +1,14 @@
 CREATE TABLE snaps
 (
-    id         integer primary key not null,
-    uuid       varchar(255),
-    usr_id     integer             not null,
-    ist_id     integer,
-    title      varchar(50)         not null,
-    active     integer default 1,
-    created_at timestamp           not null,
-    created_by varchar(45)         not null,
-    updated_at timestamp           not null,
-    updated_by varchar(45)         not null
+    id           integer primary key not null,
+    uuid         varchar(255),
+    usr_id       integer             not null,
+    ist_id       integer,
+    title        varchar(50)         not null,
+    chord_schema varchar(100),
+    active       integer default 1,
+    created_at   timestamp           not null,
+    created_by   varchar(45)         not null,
+    updated_at   timestamp           not null,
+    updated_by   varchar(45)         not null
 );
