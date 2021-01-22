ffe360ba424a24b77e4a7787829f550a6566dc02
Jan Ripke <janripke@gmail.com>
2021-01-22 19:43:33 +0100

    snaps

--- create/snapsnare/dat/local/instruments.sql
+++ create/snapsnare/dat/local/instruments.sql
@@ -1,1 +1,3 @@
 insert into instruments(uuid, name, icon_url) values (uuid_generate_v4(), 'drum', 'drum.png');
+insert into instruments(uuid, name, icon_url) values (uuid_generate_v4(), 'gitar', '');
+insert into instruments(uuid, name, icon_url) values (uuid_generate_v4(), 'synthesizer', '');
