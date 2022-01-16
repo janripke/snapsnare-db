6f8f421b0cdc71ab49c45189507c328330ff2165
Jan Ripke <janripke@gmail.com>
2022-01-16 19:14:50 +0100

    translate from Dutch to English

---
+++ alter/1.0.10/snapsnare/dat/a-sections.sql
@@ -0,0 +1,1 @@
+update sections set name='About us' where name='Over ons';
---
+++ alter/1.0.10/snapsnare/dat/templates.sql
@@ -0,0 +1,1 @@
+update templates set content='<img src=http://snapsnare.org/static/snapsnare.png><br>Your account is created.<br> Go to <a href=http://{host}/activate?uuid={uuid}>http://{host}/activate?uuid={uuid}</a>, to activate your account.' where template='activate'
